09e48cde013727bbbba4f694151ac601a9c4efe1
dacods <135403583+dacods@users.noreply.github.com>
2025-05-08 11:53:08 -0500

    React project setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
 import "./index.css";
 
 // Render the App component to the root element
+import ReactDOM from "react-dom/client"
+import App from "./App"
+
+const div = document.getElementById("root")
+const root = ReactDOM.createRoot(div)
+root.render(<App />);
