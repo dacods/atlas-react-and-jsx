388c42b3e37e25ebbfeab4b4eed6d006cc0912a4
dacods <135403583+dacods@users.noreply.github.com>
2025-05-10 15:46:00 -0500

    Created Section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import Header from "./components/Header";
+import Section from "./components/Section";
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
+      <Section title="What is react?">
+        React is a popular JavaScript library for building user interfaces,
+        particularly single-page applications where a dynamic and
+        responsive user experience is essential.
+        React uses JSX, a syntax extension that allows HTML-like code to coexist with JavaScript. 
+        JSX makes it easier to write and understand the structure of the UI components.
+        React maintains a virtual representation of the DOM, 
+        which allows it to efficiently update and render only the components that have changed, 
+        improving performance.
+      </Section>
     </div>
   )
 }
---
+++ src/components/Section.jsx
@@ -0,0 +1,10 @@
+function Section(props) {
+    return ( 
+        <div className="section">
+            <h1>{props.title}</h1>
+            {props.children}
+        </div>
+    )
+}
+
+export default Section;
